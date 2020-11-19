642e42cc2d74b07d9f51e1347d139cc264d439a6
Markus Moeller <markus.moeller@avanade.com>
2020-11-19 15:35:18 +0100

    usings

--- GraphConsoleApp/Controller/GraphController.cs
+++ GraphConsoleApp/Controller/GraphController.cs
@@ -1,8 +1,7 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
